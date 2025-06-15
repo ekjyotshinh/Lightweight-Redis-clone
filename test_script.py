cab7934626db42def04b1f9c5d6c9c2c2a328cce
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2025-06-14 21:20:23 -0700

    test script updates to account for expire time

--- test_script.py
+++ test_script.py
@@ -3,7 +3,11 @@ import threading
 import time
 
 
-def send_command(client_id, command_bytes, expected_response_bytes):
+def send_command(client_id, command_bytes, expected_response_bytes, delay_before_check=0):
+    """
+    Send command_bytes to the Redis-like server,
+    then optionally wait delay_before_check seconds before reading the response.
+    """
     proc = subprocess.Popen(
         ['nc', 'localhost', '6379'],
         stdin=subprocess.PIPE,
@@ -14,6 +18,9 @@ def send_command(client_id, command_bytes, expected_response_bytes):
     proc.stdin.write(command_bytes)
     proc.stdin.flush()
     proc.stdin.close()
+
+    if delay_before_check > 0:
+        time.sleep(delay_before_check)
 
     response = proc.stdout.read()
 
@@ -31,36 +38,39 @@ def send_command(client_id, command_bytes, expected_response_bytes):
     proc.wait()
 
 
-# RESP test cases
+# RESP test cases including expiry tests
 test_cases = [
-    # PING
-    (b"PING\r\n", b"+PONG\r\n"),
+    # Basic commands
+    (b"PING\r\n", b"+PONG\r\n", 0),
 
-    # ECHO
-    (b"*2\r\n$4\r\nECHO\r\n$5\r\nhello\r\n", b"$5\r\nhello\r\n"),
-    (b"*2\r\n$4\r\nECHO\r\n$7\r\nchatgpt\r\n", b"$7\r\nchatgpt\r\n"),
+    (b"*2\r\n$4\r\nECHO\r\n$5\r\nhello\r\n", b"$5\r\nhello\r\n", 0),
+    (b"*2\r\n$4\r\nECHO\r\n$7\r\nchatgpt\r\n", b"$7\r\nchatgpt\r\n", 0),
 
-    # Unknown command
-    (b"FOO\r\n", b"-ERR unknown command\r\n"),
+    (b"FOO\r\n", b"-ERR unknown command\r\n", 0),
 
-    # SET key value
-    (b"*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n", b"+OK\r\n"),
+    # SET and GET without expiry
+    (b"*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n", b"+OK\r\n", 0),
+    (b"*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n", b"$3\r\nbar\r\n", 0),
 
-    # GET existing key
-    (b"*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n", b"$3\r\nbar\r\n"),
+    (b"*2\r\n$3\r\nGET\r\n$6\r\nno_key\r\n", b"$-1\r\n", 0),
 
-    # GET missing key
-    (b"*2\r\n$3\r\nGET\r\n$6\r\nno_key\r\n", b"$-1\r\n"),
+    # SET with PX expiry 500ms
+    (b"*5\r\n$3\r\nSET\r\n$5\r\nhello\r\n$5\r\nworld\r\n$2\r\nPX\r\n$3\r\n500\r\n", b"+OK\r\n", 0),
+
+    # Immediately get key, should exist
+    (b"*2\r\n$3\r\nGET\r\n$5\r\nhello\r\n", b"$5\r\nworld\r\n", 0),
+
+    # Wait 600ms (> expiry) then get key, should be expired (null bulk string)
+    (b"*2\r\n$3\r\nGET\r\n$5\r\nhello\r\n", b"$-1\r\n", 0.6),
 ]
 
 threads = []
 
-for i, (cmd, expected) in enumerate(test_cases):
-    t = threading.Thread(target=send_command, args=(i + 1, cmd, expected))
+for i, (cmd, expected, delay) in enumerate(test_cases):
+    t = threading.Thread(target=send_command,
+                         args=(i + 1, cmd, expected, delay))
     t.start()
     threads.append(t)
-    # Small delay to make sure SET runs before GET
-    time.sleep(0.1)
 
 for t in threads:
     t.join()
